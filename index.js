cd9ef83ac6559643f8cfd26914afa648fd37df1a
Lucas Reis <lucasreis_cod@hotmail.com>
2022-06-19 22:42:42 -0300

    Add a conexão com o banco de dados

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 ////////// CONFIGURAÇÃO INICIAL \\\\\\\\\\
 const express = require('express')
+const mongoose = require('mongoose')
 const app = express()
+
+const Person = require('./models/Person')
 
 ////////// FORMA DE LER JSON / MIDDLEWARES \\\\\\\\\\
 app.use(
@@ -19,4 +22,12 @@ res.json({msg: "Oi Express!"})
 })
 
 ////////// ENTREGAR UMA PORTA  \\\\\\\\\\
-app.listen(3000)
+mongoose.connect('mongodb+srv://teste:teste123@cluster0.s010j.mongodb.net/?retryWrites=true&w=majority'
+).then(() =>{
+    console.log(">>>>>> CONECTADO AO MONGO <<<<<<")
+    app.listen(3000)
+})
+.catch((error) => console.log(error))
+
+
+
