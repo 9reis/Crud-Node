af1d3343966f34330294a1467fbcf839244767a4
Lucas Reis <lucasreis_cod@hotmail.com>
2022-06-19 23:21:09 -0300

    Add a rota Create

--- index.js
+++ index.js
@@ -5,6 +5,9 @@ const app = express()
 
 const Person = require('./models/Person')
 
+///////////////////////// 50:30
+
+
 ////////// FORMA DE LER JSON / MIDDLEWARES \\\\\\\\\\
 app.use(
     express.urlencoded({
@@ -13,12 +16,42 @@ app.use(
 )
 app.use(express.json())
 
+////////// ROTAS DA API \\\\\\\\\\
+app.post('/person', async (req,res)=> {
+
+        /// req.body \\\
+    const {name,salary,approved} = req.body
+
+    if(!name){
+        res.status(422).json({error:"O nome é Obrigatório"})
+    } else if(!salary){
+        res.status(422).json({error:"O salario é Obrigatório"})
+    }
+
+    const person = {
+        name,
+        salary,
+        approved
+    }
+
+        /// CREATE \\\
+    try{
+        // Criando os dados
+        await Person.create(person)
+
+        res.status(201).json({msg: "Pessoa inserida no sistema com sucesso"})
+
+    }catch(error){
+        res.status(500).json({Error:error})
+    }
+
+})
+
 ////////// ROTA INICIAL || ENDPOINT \\\\\\\\\\
 app.get('/', (req,res) => {
 
         /// mostrar req \\\
-
-res.json({msg: "Oi Express!"})
+    res.json({msg: "Oi Express!"})
 })
 
 ////////// ENTREGAR UMA PORTA  \\\\\\\\\\
